84633d2e5ece99f5eff230d6fb8fc1553a006e66
Cosmin-Mihai Tutunaru <cosmin@tutunaru.ro>
2014-07-12 23:52:48 +0300

    Update Twitter search to use TweetFactory.

--- Service/Search.php
+++ Service/Search.php
@@ -2,25 +2,80 @@
 
 namespace Stocarul\TwitterBundle\Service;
 
-use Stocarul\TwitterBundle\Service\SearchInterface;
 use Stocarul\TwitterBundle\Http\ClientInterface;
+use Stocarul\TwitterBundle\Factory\TweetFactory;
 
+/**
+ * Class: Search
+ *
+ * @see SearchInterface
+ */
 class Search implements SearchInterface
 {
+    /**
+     * Http client
+     *
+     * @var ClientInterface
+     */
     protected $client;
 
+    /**
+     * tweetFactory
+     *
+     * @var TweetFactory
+     */
+    protected $tweetFactory;
+
+    /**
+     * Search constructor
+     *
+     * @param ClientInterface $client
+     */
     public function __construct(ClientInterface $client)
     {
         $this->client = $client;
     }
 
+    /**
+     * Get tweetFactory
+     *
+     * @return TweetFactory
+     */
+    public function getTweetFactory()
+    {
+        return $this->tweetFactory;
+    }
+
+    /**
+     * Set tweetFactory
+     *
+     * @param  TweetFactory $tweetFactory
+     * @return Search
+     */
+    public function setTweetFactory(TweetFactory $tweetFactory)
+    {
+        $this->tweetFactory = $tweetFactory;
+
+        return $this;
+    }
+
+    /**
+     * Returns a collection of relevant Tweets matching a specified query.
+     *
+     * @param  array   $params
+     * @return Tweet[]
+     */
     public function searchTweets($params = array())
     {
-        return $this->client->get(
+        $response = $this->client->get(
             'search/tweets.json',
             array(
                 'query' => $params,
             )
         );
+
+        $response = json_decode($response->getBody());
+
+        return $this->getTweetFactory()->constructArray($response->statuses);
     }
 }
--- Service/SearchInterface.php
+++ Service/SearchInterface.php
@@ -3,8 +3,42 @@
 namespace Stocarul\TwitterBundle\Service;
 
 use Stocarul\TwitterBundle\Http\ClientInterface;
+use Stocarul\TwitterBundle\Factory\TweetFactory;
 
+/**
+ * Class: SearchInterface
+ *
+ */
 interface SearchInterface
 {
+    /**
+     * Search constructor
+     *
+     * @param ClientInterface $client
+     */
     public function __construct(ClientInterface $client);
+
+    /**
+     * Get tweetFactory
+     *
+     * @return TweetFactory
+     */
+    public function getTweetFactory();
+
+    /**
+     * Set tweetFactory
+     *
+     * @param  TweetFactory    $tweetFactory
+     * @return SearchInterface
+     */
+    public function setTweetFactory(TweetFactory $tweetFactory);
+
+    /**
+     * Returns a collection of relevant Tweets matching a specified query.
+     *
+     * @param array $params
+     *
+     * @return Tweet[]
+     */
+    public function searchTweets($params = array());
 }
