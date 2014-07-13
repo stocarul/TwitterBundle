25b4535985642368ac17eb9a31f461be46ab17fd
Cosmin-Mihai Tutunaru <cosmin@tutunaru.ro>
2014-07-13 22:12:36 +0300

    Add documentation to the http classes.

--- Http/Client.php
+++ Http/Client.php
@@ -4,10 +4,20 @@ namespace Stocarul\TwitterBundle\Http;
 
 use GuzzleHttp\Client AS Base;
 use GuzzleHttp\Event\SubscriberInterface;
-use Stocarul\TwitterBundle\Http\ClientInterface;
 
+/**
+ * Class: Client
+ *
+ * @see ClientInterface
+ * @see Base
+ */
 class Client extends Base implements ClientInterface
 {
+    /**
+     * Attach a subscriber
+     *
+     * @param SubscriberInterface $subscriber
+     */
     public function addSubscriber(SubscriberInterface $subscriber)
     {
         $this->getEmitter()->attach($subscriber);
--- Http/ClientInterface.php
+++ Http/ClientInterface.php
@@ -5,7 +5,17 @@ namespace Stocarul\TwitterBundle\Http;
 use GuzzleHttp\ClientInterface AS Base;
 use GuzzleHttp\Event\SubscriberInterface;
 
+/**
+ * Class: ClientInterface
+ *
+ * @see Base
+ */
 interface ClientInterface extends Base
 {
+    /**
+     * Attach a subscriber
+     *
+     * @param SubscriberInterface $subscriber
+     */
     public function addSubscriber(SubscriberInterface $subscriber);
 }
