4dbd08d4117fbf35562781784c5860710fa52802
Cosmin-Mihai Tutunaru <cosmin@tutunaru.ro>
2014-07-15 21:12:45 +0300

    Enable Functional testing.

---
+++ Tests/Fixtures/App/AppKernel.php
@@ -0,0 +1,53 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+/**
+ * Class: AppKernel
+ *
+ * @see Kernel
+ */
+class AppKernel extends Kernel
+{
+    /**
+     * @return array
+     */
+    public function registerBundles()
+    {
+        return array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Stocarul\TwitterBundle\StocarulTwitterBundle(),
+        );
+    }
+
+    /**
+     * registerContainerConfiguration
+     *
+     * @param LoaderInterface $loader
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config.yml');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        return sys_get_temp_dir().'/StocarulTwitterBundle/cache';
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogDir()
+    {
+        return sys_get_temp_dir().'/StocarulTwitterBundle/logs';
+    }
+}
