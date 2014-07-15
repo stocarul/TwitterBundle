6187706ff7d7ba2887786176e55272a32f9dabc4
Cosmin-Mihai Tutunaru <cosmin@tutunaru.ro>
2014-07-15 17:44:19 +0300

    Update SearchTest.php.

--- Tests/Service/SearchTest.php
+++ Tests/Service/SearchTest.php
@@ -2,28 +2,156 @@
 
 namespace Stocarul\TwitterBundle\Tests\Service;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Stocarul\TwitterBundle\Service\Search;
 
-class SearchTest extends WebTestCase
+/**
+ * Class: SearchTest
+ *
+ * @see \PHPUnit_Framework_TestCase
+ */
+class SearchTest extends \PHPUnit_Framework_TestCase
 {
-    protected function getContainer()
-    {
-        $kernel = $this->createKernel();
-        $kernel->boot();
-
-        return $kernel->getContainer();
-    }
-
+    /**
+     * testSearch
+     *
+     */
     public function testSearch()
     {
-        $obj = $this->getContainer()->get('stocarul_twitter.search');
+        $client = $this->getClient();
+        $tweetFactory = $this->getTweetFactory();
 
-        $response = $obj->searchTweets(array(
-            'q' => '#worldcup',
-        ));
+        $searchService = new Search($client, $tweetFactory);
 
-        $body = $response->getBody();
-        var_dump(json_decode($body));
+        $tweets = $searchService->searchTweets($this->getQuery());
+        $this->assertEquals($this->getResults(), $tweets);
+    }
+
+    /**
+     * getTweetFactory
+     *
+     * @return \Stocarul\TwitterBundle\Factory\TweetFactory
+     */
+    public function getTweetFactory()
+    {
+        $factory = $this
+            ->getMockBuilder('\Stocarul\TwitterBundle\Factory\TweetFactory')
+            ->setMethods(array('constructArray'))
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $factory
+            ->expects($this->once())
+            ->method('constructArray')
+            ->with($this->equalTo($this->getStatuses()))
+            ->will($this->returnValue($this->getResults()))
+        ;
+
+        return $factory;
+    }
+
+    /**
+     * getClient
+     *
+     * @return \Stocarul\TwitterBundle\Http\Client
+     */
+    public function getClient()
+    {
+        $client = $this
+            ->getMockBuilder('\Stocarul\TwitterBundle\Http\Client')
+            ->setMethods(array('get'))
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $client
+            ->expects($this->once())
+            ->method('get')
+            ->with(
+                $this->equalTo('search/tweets.json'),
+                $this->equalTo(array(
+                    'query' => $this->getQuery(),
+                ))
+            )
+            ->will($this->returnValue($this->getClientResponse()))
+        ;
+
+        return $client;
+    }
+
+    /**
+     * getClientResponse
+     *
+     * @return \GuzzleHttp\Message\Response
+     */
+    public function getClientResponse()
+    {
+        $response = $this
+            ->getMockBuilder('\GuzzleHttp\Message\Response')
+            ->setMethods(array('getBody'))
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $response
+            ->expects($this->once())
+            ->method('getBody')
+            ->with()
+            ->will($this->returnValue($this->getResponseJson()))
+        ;
+
+        return $response;
+    }
+
+    /**
+     * The query sent to the searchTweets method
+     *
+     * @erturn array
+     */
+    public function getQuery()
+    {
+        return array(
+            'q' => '#hashtag',
+        );
+    }
+
+    /**
+     * The expected response form the server
+     *
+     * @return string
+     */
+    public function getResponseJson()
+    {
+        $response = array(
+            'statuses' => $this->getStatuses(),
+        );
+
+        return json_encode($response);
+    }
+
+    /**
+     * getStatuses
+     *
+     * @return string[]
+     */
+    public function getStatuses()
+    {
+        return array(
+            'a',
+            'b',
+        );
+    }
+
+    /**
+     * getResults
+     *
+     * @return string[]
+     */
+    public function getResults()
+    {
+        return array(
+            'x',
+            'y',
+        );
     }
 }
