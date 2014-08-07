56a745e38cd1697e20f95a3b96b83df6b97949dd
Cosmin-Mihai Tutunaru <cosmin@tutunaru.ro>
2014-08-07 09:53:25 +0300

    Use getenv for environment variables.

--- Tests/Fixtures/App/config/parameters.php
+++ Tests/Fixtures/App/config/parameters.php
@@ -1,9 +1,9 @@
 <?php
 
-if (isset($_ENV['OAUTH_CONSUMER_KEY'])) {
-    $container->setParameter('oauth_consumer_key', $_ENV['OAUTH_CONSUMER_KEY']);
+if (getenv('OAUTH_CONSUMER_KEY')) {
+    $container->setParameter('oauth_consumer_key', getenv('OAUTH_CONSUMER_KEY'));
 }
 
-if (isset($_ENV['OAUTH_CONSUMER_SECRET'])) {
-    $container->setParameter('oauth_consumer_secret', $_ENV['OAUTH_CONSUMER_SECRET']);
+if (getenv('OAUTH_CONSUMER_SECRET')) {
+    $container->setParameter('oauth_consumer_secret', getenv('OAUTH_CONSUMER_SECRET'));
 }
