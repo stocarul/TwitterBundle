51df6db1469b23eb1e9717e516057a9d5765de90
Cosmin-Mihai Tutunaru <cosmin@tutunaru.ro>
2014-08-07 09:41:02 +0300

    Configure shippable ci service.

--- Tests/Fixtures/App/config/parameters.php
+++ Tests/Fixtures/App/config/parameters.php
@@ -1,1 +1,9 @@
 <?php
+
+if (isset($_ENV['OAUTH_CONSUMER_KEY'])) {
+    $container->setParameter('oauth_consumer_key', $_ENV['OAUTH_CONSUMER_KEY']);
+}
+
+if (isset($_ENV['OAUTH_CONSUMER_SECRET'])) {
+    $container->setParameter('oauth_consumer_secret', $_ENV['OAUTH_CONSUMER_SECRET']);
+}
